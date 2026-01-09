b86854396d31bb842c4c12f3fd802f893a26d565
Wolnei <Wolnei@DESKTOP-V1C8S3V>
2026-01-09 16:30:45 -0300

    4º Commit - nº73 - Exer.2

---
+++ course_73_exr_2/src/aplication/Program.java
@@ -0,0 +1,37 @@
+package aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		Employee emp = new Employee();
+
+		System.out.print("Name: ");
+		emp.name = sc.nextLine();
+
+		System.out.print("Gross salary: ");
+		emp.grossSalary = sc.nextDouble();
+
+		System.out.print("Tax: ");
+		emp.tax = sc.nextDouble();
+
+		System.out.println();
+		System.out.println("Employee: " + emp.toString());
+
+		System.out.println();
+		System.out.println("Which percentage to increase salary?");
+		emp.increaseSalary(sc.nextDouble());
+
+		System.out.println();
+		System.out.println("Updated data: " + emp.toString());
+		sc.close();
+	}
+
+}
